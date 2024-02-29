b9f058adb5cb45475bca2f3023e0a05606620ff4
TarasHrynchuk <125279218+TarasHrynchuk@users.noreply.github.com>
2024-02-29 17:44:30 +0100

    v1.0.1

---
+++ dependency/dependency.go
@@ -0,0 +1,7 @@
+package dependency
+
+import "fmt"
+
+func PrintDependencyMessage() {
+	fmt.Println("Dependency. Release v1.0.1")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/my-org-for-test/go-repo-out
+
+go 1.22
